f3088b0ddf52349d9bcebe16643e1ad70e73b650
Junior Santos <nuno.ribeiro05@hotmail.com>
2023-06-19 00:11:59 -0300

    feat: modal successfully_created implemementado

--- src/contexts/userContext.tsx
+++ src/contexts/userContext.tsx
@@ -17,6 +17,8 @@ interface IUserContext {
   anuncio: (anuncioData: TAnuncioData) => Promise<void>
   userData: IUser | null
   setUserData: Dispatch<SetStateAction<IUser | null>>
+  successfullyCreated: boolean
+  setSuccessfullyCreated: Dispatch<SetStateAction<boolean>>
 }
 
 interface IUser {
@@ -64,13 +66,14 @@ export const UserContext = createContext({} as IUserContext);
 
 const UserProvider = ({ children }: IUserProviderProps) => {
   const [userData, setUserData] = useState<IUser | null>(null);
+  const [successfullyCreated, setSuccessfullyCreated] = useState(false)
   const navigate = useNavigate();
 
   const userRegister = async (userData: TRegisterData): Promise<void> => {
     try {
       const response = await ApiShop.post<IUser>("/users", userData);
       setUserData(response.data)
-      navigate("/login")
+      setSuccessfullyCreated(true)
     } catch (error) {
       const axiosError = error as AxiosError;
       console.log(axiosError.message)
@@ -98,7 +101,7 @@ const UserProvider = ({ children }: IUserProviderProps) => {
   };
 
   return (
-    <UserContext.Provider value={{ userRegister, login, anuncio, userData, setUserData }}>
+    <UserContext.Provider value={{ userRegister, login, anuncio, userData, setUserData, successfullyCreated, setSuccessfullyCreated }}>
       {children}
     </UserContext.Provider>
   );
