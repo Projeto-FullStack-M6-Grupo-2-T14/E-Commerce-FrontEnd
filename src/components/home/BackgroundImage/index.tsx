9a200bb8a37a2290a065458e7d549b0f290695ec
Christiano Oliveira <106622459+Christiano-K-Oliveira@users.noreply.github.com>
2023-06-22 13:43:37 -0300

    Update index.tsx

--- src/components/home/BackgroundImage/index.tsx
+++ src/components/home/BackgroundImage/index.tsx
@@ -8,8 +8,8 @@ const BackgroundImage = ({ isMobileMenuOpen }: IBackgroundImageProps) => {
   return (
     <div className={styles.container}>
       <div className={`${styles.gradient} ${isMobileMenuOpen ? styles.open : ''}`}  >
-        <h1 className="heading-3-500">Motors Shop</h1>
-        <h3 className="heading-5-500">A melhor plataforma de anúncios de carros do país</h3>
+        <h1 className="heading-2-600">Motors Shop</h1>
+        <h3 className="heading-4-500">A melhor plataforma de anúncios de carros do país</h3>
       </div>
     </div>
   );
