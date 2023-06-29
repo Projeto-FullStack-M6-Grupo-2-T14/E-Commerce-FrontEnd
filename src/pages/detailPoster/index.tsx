5032999f31b9d58712a74b163442ad5b1ea622df
renangiaretta <renan.giaretta@hotmail.com>
2023-06-29 19:14:47 -0300

    feat: create comments and product details

---
+++ src/pages/detailPoster/index.tsx
@@ -0,0 +1,26 @@
+import Header from 'src/components/home/Header'
+import './detailPoster.module.sass'
+
+
+const DetailPoster = () => {
+    return (
+        <>
+            <Header />
+            <main>
+                <div>
+                    <div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <form></form>
+                    </div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default DetailPoster
