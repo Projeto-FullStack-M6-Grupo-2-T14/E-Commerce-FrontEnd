aa81d4b7ee5f6958c868301b04ad623a53e155d9
pethe <pethersonreis@gmail.com>
2023-06-30 16:49:53 -0300

    update reset pass inputs

--- src/pages/newPassword/index.tsx
+++ src/pages/newPassword/index.tsx
@@ -1,15 +1,18 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { UserContext } from "src/contexts/userContext";
 import { Tooltip } from "react-tooltip";
 import { TNewPass, newPassSchema } from "./newPasswordSchema";
 
 import styles from "./newPassword.module.sass";
+import { FiEye, FiEyeOff } from "react-icons/fi";
 
 const NewPasswordPage = () => {
-  const { updatePassword } = useContext(UserContext)
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
+  const { updatePassword } = useContext(UserContext)
 
   const { register, handleSubmit, formState: { errors } } = useForm<TNewPass>({
     resolver: zodResolver(newPassSchema),
@@ -26,43 +29,60 @@ const NewPasswordPage = () => {
 
         <div className={styles.inputBox}>
           <label htmlFor="newPass">Nova Senha</label>
-          <input
-            type="text"
-            placeholder="Digite uma nova senha aqui"
-            id="newPass"
-            autoComplete="given-newPass"
-            {...register("newPass")}
-            data-tooltip-id="newPass-tooltip"
-            data-tooltip-content={errors.newPass ? errors.newPass.message : ""}
-            data-tooltip-place="top"
-            data-tooltip-float={true}
-          />
-          <Tooltip id="newPass-tooltip" />
+          <div className={styles.passwordInput}>
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Digitar senha"
+              id="newPass"
+              autoComplete="given-newPass"
+              {...register("newPass")}
+              data-tooltip-id="newPass-tooltip"
+              data-tooltip-content={
+                errors.newPass ? errors.newPass.message : ""
+              }
+              data-tooltip-place="top"
+              data-tooltip-float={true}
+            />
+            {showPassword ? (
+              <FiEyeOff onClick={() => setShowPassword(false)} />
+            ) : (
+              <FiEye onClick={() => setShowPassword(true)} />
+            )}
+          </div>
+          <Tooltip id="newPass-tooltip" className={styles.tooltip} />
         </div>
 
         <div className={styles.inputBox}>
           <label htmlFor="confirmNewPass">Confirmar nova senha</label>
-          <input
-            type="text"
-            placeholder="Repita a senha aqui"
-            id="confirmNewPass"
-            autoComplete="given-confirmNewPass"
-            {...register("confirmNewPass")}
-            data-tooltip-id="confirmNewPass-tooltip"
-            data-tooltip-content={
-              errors.confirmNewPass ? errors.confirmNewPass.message : ""
-            }
-            data-tooltip-place="top"
-            data-tooltip-float={true}
-          />
-          <Tooltip id="confirmNewPass-tooltip" />
+          <div className={styles.passwordInput}>
+            <input
+              type={showConfirmPassword ? "text" : "password"}
+              placeholder="Digitar senha"
+              id="confirmNewPass"
+              autoComplete="given-confirmNewPass"
+              {...register("confirmNewPass")}
+              data-tooltip-id="confirmNewPass-tooltip"
+              data-tooltip-content={
+                errors.confirmNewPass ? errors.confirmNewPass.message : ""
+              }
+              data-tooltip-place="top"
+              data-tooltip-float={true}
+            />
+            <Tooltip id="confirmNewPass-tooltip" className={styles.tooltip} />
+            {showConfirmPassword ? (
+              <FiEyeOff onClick={() => setShowConfirmPassword(false)} />
+            ) : (
+              <FiEye onClick={() => setShowConfirmPassword(true)} />
+            )}
+          </div>
+          <Tooltip id="confirmNewPass-tooltip" className={styles.tooltip} />
         </div>
 
         <button type="submit" className={styles.btnUpdate}>
           Alterar
         </button>
       </form>
-    </main>
+    </main >
   );
 };
 
